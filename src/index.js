07eed26582a4b3f9108b8a01cfa7debb915052d5
Belastotski <di.belost@gmail.com>
2022-05-09 02:13:32 +0200

    feat: add description

--- src/index.js
+++ src/index.js
@@ -6,14 +6,34 @@ import Controller from './controller.js';
 customElements.define('v-key', Key);
 customElements.define('v-text', Text);
 
+const html = document.querySelector('html');
+const head = document.querySelector('head');
 const app = document.getElementById('app');
 const container = document.createElement('div');
 const text = document.createElement('v-text');
 const controller = new Controller(Key, text);
+const desc = document.createElement('div');
+desc.innerHTML = `
+</br>Keyboard was created in Windows. 
+</br> Switch between language LeftShift + LeftAlt.
+</br> Auto keys repeat off.
+`;
+desc.style.margin = 'auto';
+desc.style.maxWidth = '1200px';
+desc.style.minWidth = '810px';
 
+head.innerHTML = `
+<meta charset="UTF-8" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<link rel="stylesheet" href="./style.css" />
+<title>Keyboard</title>
+`;
+html.setAttribute('lang', 'en');
 app.append(container);
 container.append(text);
 container.append(keyboard);
+container.append(desc);
 
 app.onblur = Key.sysClear;
 
